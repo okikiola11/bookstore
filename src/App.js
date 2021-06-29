eb7808ccfebf1cde5bf96594bb9aa31c4d75a057
okikiola11 <okikiola.apelehin@gmail.com>
2021-06-29 13:03:34 +0000

    Add the react-redux and redux dependency package Add the necessary eslint, stylelint dependency package Add the proptypes package Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header" />
+  </div>
+);
 
 export default App;
