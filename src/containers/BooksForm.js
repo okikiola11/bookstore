40643d2a1cff645ad79dff371c0e985ca7e94ca4
okikiola11 <okikiola.apelehin@gmail.com>
2021-07-07 18:41:47 +0000

    Make BookForm Component stateful The component stores the title and the book category in its own state The component implements the handleChange method which reacts to the user typing to input a name or to changes in the category select Each change to the form modifies the component’s state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,21 +1,59 @@
 import React from 'react';
 
-const BOOKSCATEGORY = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: 'Action',
+    };
 
-const BooksForm = () => (
-  <form>
-    <div>
-      <input type="text" />
-    </div>
-    <div>
-      <select>
-        {BOOKSCATEGORY.map((option) => (
-          <option key={option.id} value={option}>{option}</option>
-        ))}
-      </select>
-    </div>
-    <button type="submit">Submit</button>
-  </form>
-);
+    const BOOKSCATEGORY = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelectOption = this.handleSelectOption.bind(this);
+  }
+
+  handleInputChange = (event) => {
+    console.log('Option Selected!!');
+    const { value } = event.target;
+    const { name } = event.target.name;
+
+    this.setState({ 
+      [name]: value
+     });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit Selected!!');
+    this.setState({ title: e.target.value });
+  };
+
+  render() {
+    const { title, category } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+          name="textInput"
+          type="text" 
+          value={title}
+          onChange={this.handleInputChange}
+          />
+        </div>
+        <div>
+          <select name="selectbox" value={category} onChange={this.handleInputChange}>
+            {BOOKSCATEGORY.map((option) => (
+              <option key={option.id} value={option}>{option}</option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 export default BooksForm;
