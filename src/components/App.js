614a74c28e2d4caad1f450c03762d1146828e6a2
okikiola11 <okikiola.apelehin@gmail.com>
2021-08-17 21:31:50 +0000

    Add the Nav Component to Components Add styling for the Nav.js Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import NavBar from './Nav';
 import '../App.css';
 
 const App = () => (
   <div className="App">
     <>
+      <NavBar />
       <BooksList />
       <BooksForm />
     </>
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import icon from '../assets/user.svg';
+
+const NavBar = () => (
+  <nav className="">
+    <div className="panel-bg">
+      <ul>
+        <li className="azure Bookstore-CMS">BOOKSTORE CMS</li>
+        <li className="BOOKS">BOOKS</li>
+        <li className="CATEGORIES">CATEGORIES</li>
+        <li>
+          <img alt="icon" src={icon} className="azure Mask" />
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default NavBar;
