a4d9055743b14f1417ab556ada5b4fd869df7f23
okikiola11 <okikiola.apelehin@gmail.com>
2021-08-19 23:46:53 +0000

    Update the css styling for the Book.js Component

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,24 +5,38 @@ const Book = ({ book, handleRemoveBook }) => {
   const { id, title, category } = book;
 
   return (
-    <tr className="Lesson-Panel">
-      <div className="display">
-        <div className="book-info">
-          <td className="book-id">{id}</td>
-          <td className="book-title">{title}</td>
-          <td className="book-cat">{category}</td>
-          <div className="options">
-            <td className="Remove comment">Comments</td>
-            <td>
+    <div className="lesson-panel">
+      <div>
+        <div className="book-cat">{category}</div>
+        <div className="book-title">{title}</div>
+        <div className="author-name">Author Name</div>
+        <div className="options">
+          <ul>
+            <li className="book-comment">Comments</li>
+            <li className="book-remove">
               <button type="button" onClick={() => handleRemoveBook(book)}>Delete</button>
-            </td>
+            </li>
+            <li className="book-edit">Edit</li>
+          </ul>
+        </div>
+        <div className="display book-progress">
+          <div className="circle" />
+          <div classsName="percent-info">
+            <span className="percentage">75%</span>
+            <br />
+            <span className="completed">Completed</span>
           </div>
         </div>
-        <div className="book-progress">
-          <div className="circle" />
+        <div className="udpate-progress">
+          <p className="current-chapter">current chapter</p>
+          <p className="chapter-number">
+            Chapter
+            {id}
+          </p>
+          <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
-    </tr>
+    </div>
   );
 };
 
